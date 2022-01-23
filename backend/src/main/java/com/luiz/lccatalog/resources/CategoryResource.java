227c81cb4866aa77c7bfbd72c5b0d4463d7aa527
Luiz <lcmscontas@gmail.com>
2022-01-23 12:48:02 -0300

    Find Category by id, execption handling;

--- backend/src/main/java/com/luiz/lccatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/luiz/lccatalog/resources/CategoryResource.java
@@ -6,11 +6,12 @@ import com.luiz.lccatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -20,7 +21,12 @@ public class CategoryResource {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll(){
+    public ResponseEntity<List<CategoryDTO>> findAll() {
         return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
--- backend/src/main/java/com/luiz/lccatalog/services/CategoryService.java
+++ backend/src/main/java/com/luiz/lccatalog/services/CategoryService.java
@@ -3,8 +3,10 @@ package com.luiz.lccatalog.services;
 import com.luiz.lccatalog.dto.CategoryDTO;
 import com.luiz.lccatalog.entities.Category;
 import com.luiz.lccatalog.repositories.CategoryRepository;
+import com.luiz.lccatalog.services.execeptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,8 +17,15 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repo;
 
+    @Transactional(readOnly = true)
     public List<CategoryDTO> findAll() {
         List<Category> list = repo.findAll();
-        return list.stream().map( x -> new CategoryDTO(x)).collect(Collectors.toList());
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        return new CategoryDTO(repo.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Entity not found")));
     }
 }
